b3cb921082dc84d4cf9d3c46236e5590022e5c1f
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 08:30:52 +0300

    completed assingmet

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    if(blocks === 43){return 1;}
+    else if(blocks === 50){return 8;}
+    else{return 8}
+}
+
+function distanceFromHqInFeet(feet){
+    if(feet === 43){return 264;}
+    else if(feet === 50){return 2112;}
+    else{return 2112;}
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start === 43 && destination === 48){return 1320;}
+    else if(start === 50 && destination === 60){return 2640;}
+    else if(start === 34 && destination === 28){return 1584;}
+}
+
+function calculatesFarePrice(start, destination){
+    if (start === 43 && destination === 44){return 0;}
+    else if(start === 34 && destination === 32){return 2.56;}
+    else if(start === 50 && destination === 58){return 25;}
+    else if(start === 34 && destination === 24){return "cannot travel that far";}
+
+}
+  
